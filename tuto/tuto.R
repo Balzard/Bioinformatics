b2af0c11370f1dcaadf5ae0fca8d41e38b5569b5
Balzard <stephane.leblanc@student.unamur.be>
2022-10-02 22:55:57 +0200

    tuto almost finished

--- tuto/tuto.R
+++ tuto/tuto.R
@@ -1,13 +1,39 @@
-nb_sliding_windows <- function(l, size, shift){
-    tmp = size
-    counter = 1
-    while(tmp < l){
-        tmp = tmp + shift
-        counter = counter + 1
+library(ape)
+
+nb_sliding_windows <- function(l, size, shift) {
+    tmp <- size
+    counter <- 1
+    while (tmp < l) {
+        tmp <- tmp + shift
+        counter <- counter + 1
     }
     counter
 }
 
-x = nb_sliding_windows(44,11,2)
+count <- function (s, c) { # sequence, character
+    length(which(s == c))
+}
+
+
+frequency_in_sliding_window <- function(sequence, ntd, size, shift) {
+    vec = vector(length = nb_sliding_windows(length(sequence), size, shift))
+    start = 1
+    end = size
+    for(i in 1:length(vec)) {
+        if(i==length(vec)) {
+            vec[i] = count(sequence[start:length(sequence)], ntd) / length(sequence[start:end])
+        }
+        else {
+            vec[i] = count(sequence[start:end], ntd) / length(sequence[start:end])
+        }
+        start = start + shift
+        end = end + shift
+    
+    }
+    vec
+}
+
+v = c("C", "A", "T", "A", "C", "C", "T", "T", "C", "C", "A", "A", "A")
+x = frequency_in_sliding_window(v, "T", 6, 6)
 x
 
