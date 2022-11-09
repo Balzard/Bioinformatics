fcf923d27a497417d36bcf588750495a0b02e0c8
Mathias <mathias.marques@student.uclouvain.be>
2022-11-09 19:05:23 +0100

    Add existing file

--- Projet2/main.R
+++ Projet2/main.R
@@ -1,10 +1,36 @@
 library(ape)
 
 dlAndSaveData = function(){
-    accessionsNumbers = c(readLines("./Projet2/AccessionNumbers.txt"))
+    accessionsNumbers = c(readLines("./AccessionNumbers.txt"))
     mat = read.GenBank(accessionsNumbers)
-    write.dna(mat, file = "./Projet2/data.fasta", format = "fasta", append = FALSE, nbcol = 6, colsep = " ", colw = 10)
+    write.dna(mat, file = "./data.fasta", format = "fasta", append = FALSE)
 }
 
-sequences = read.dna(file = "./Projet2/data.fasta", format = "fasta")
-print(length(sequences[1,]))
+print(unlist(sequences[1]))
+
+
+uniqchars <- function(x) unique(strsplit(x, "")[[1]]) 
+matequal <- function(x, y)
+  is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+
+
+sequences = read.dna(file = "data.fasta", format = "fasta")
+x=0
+for (i in 1:nrow(sequences)) {
+  if(length(uniqchars(c2s(sequences[i,1:3822])))!=4){
+    x=c(x,i)
+    print(uniqchars(c2s(sequences[i,1:3822])))
+  }
+  a=i+1
+  while(a<=100){
+    if(matequal(sequences[i,1:3822],sequences[a,1:3822])){
+      x=c(x,i)
+      print("same")
+    }
+    a=a+1
+    print(a)
+  }
+}
+print(x)
+sequences=sequences[-x,]
+print(sequences)
