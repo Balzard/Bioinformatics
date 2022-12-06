1f4d536f3e06a8d732272eda396a4bdb20d89590
Balzard <steph-wormi@hotmail.com>
2022-12-06 11:43:53 +0100

    part 4 done

--- Projet3/main.R
+++ Projet3/main.R
@@ -39,4 +39,69 @@ differential_selection = function(data, target, alpha){
 
 subset_train = subset_df(train)
 i = differential_selection(subset_train, "labels", 0.05)
-i
+
+
+correct = function(data, target, alpha, method){
+    features = names(data)[1:(length(names(data)) - 1)]
+    counter = 0
+    nb_tests = length(features)
+    bonferroni_correction = alpha / nb_tests
+    p_values = vector()
+    bonferroni_features = vector()
+
+    for(feature in features){
+        feature1 = subset(data[data[, target] == "No tumor",], select = feature)
+        feature2 = subset(data[data[, target] == "Glioblastoma",], select = feature)
+        test = t.test(feature1, feature2)
+        p_value = test$p.value
+
+        if(method == "bonferroni" && p_value <= bonferroni_correction){
+            p_values = append(p_values, p_value)
+            bonferroni_features = append(bonferroni_features, feature)
+        }
+
+        if(method == "fdr"){
+            p_values = append(p_values, p_value)
+        }
+    }
+    if(method == "bonferroni"){
+        names(p_values) = bonferroni_features
+        p_values = sort(p_values)
+        return(p_values)
+    }
+    if(method == "fdr"){
+        names(p_values) = features
+        p_values = sort(p_values)
+        for(i in 1:length(p_values)){
+            test = (p_values[i] * nb_tests) / i
+            if(test >= alpha){
+                p_values = p_values[1:(i - 1)]
+                return(p_values)
+            }
+        }
+    }
+}
+
+bonferroni_values = correct(subset_train, "labels", 0.05, "bonferroni")
+saveRDS(bonferroni_values, "./Projet3/bonferroni_values.rds")
+fdr_values = correct(subset_train, "labels", 0.05, "fdr")
+saveRDS(fdr_values, "./Projet3/fdr_values.rds")
+
+
+getMostDiffFeatures = function(data, target, limit=50){
+    features = names(data)[1:(length(names(data)) - 1)]
+    p_values = vector()
+    for(feature in features){
+        feature1 = subset(data[data[, target] == "No tumor",], select = feature)
+        feature2 = subset(data[data[, target] == "Glioblastoma",], select = feature)
+        test = t.test(feature1, feature2)
+        p_value = test$p.value
+        p_values = append(p_values, p_value)
+    }
+    names(p_values) = features
+    p_values = sort(p_values)
+    return(p_values[1:limit])
+}
+
+most_diff_features = getMostDiffFeatures(subset_train, "labels")
+saveRDS(most_diff_features, "./Projet3/most_diff_features.rds")
